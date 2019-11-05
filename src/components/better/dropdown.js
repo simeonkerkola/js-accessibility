8237d22044595b2aa57e19df4c2cd16c313e9f3a
Simeon Kerkola <simeon.kerkola@siilim180516.local>
2019-11-05 14:41:41 +0200

    Dropdown

--- src/components/better/dropdown.js
+++ src/components/better/dropdown.js
@@ -1,15 +1,67 @@
-import React, { useState, useRef, useEffect } from "react"
-import uuid from "uuid"
+import React, { useState, useRef, useEffect } from 'react';
 
-import "./dropdown.scss"
+import './dropdown.scss';
 
-const Dropdown = ({ }) => {
-    return (
-        <div
-            className="dropdown-wrap"
-        >
+const Dropdown = ({ activatorText, items = [] }) => {
+  const activatorRef = useRef(null);
+  const listRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
-        </div>
-    )
-}
-export default Dropdown
+  useEffect(() => {
+    if (isOpen) {
+      // set focus on the first lists item
+      listRef.current.querySelector('a').focus();
+
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+  }, [isOpen]);
+
+  const handleClick = e => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClickOutside = e => {
+    if (
+      listRef.current.contains(e.target) ||
+      activatorRef.current.contains(e.target)
+    ) {
+      return;
+    }
+    setIsOpen(false);
+  };
+
+  const handleKeyUp = e => {
+    if (e.key === 'Escape' && isOpen) setIsOpen(false);
+  };
+
+  return (
+    <div className="dropdown-wrap">
+      <button
+        aria-haspopup="true"
+        aria-controls="dropdown1"
+        ref={activatorRef}
+        className="dropdown-activator"
+        onClick={handleClick}
+        onKeyUp={handleKeyUp}
+      >
+        button
+      </button>
+      <ul
+        id="dropdown1"
+        ref={listRef}
+        className={`dropdown-itemList ${isOpen ? 'active' : ''}`}
+      >
+        {items.map((item, index) => {
+          return (
+            <li key={index}>
+              <a href={item.url}>{item.text}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+export default Dropdown;
